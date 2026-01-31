f5440d85f8b0d7e9ae2463a9982139892a249f59
alfonsoarcidiaconoeverli-dotcom <alfonsoarcidiaconoeverli@gmail.com>
2026-01-31 20:49:11 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.org.tictactoe"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.org.tictactoe"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "4.0.0"
 
