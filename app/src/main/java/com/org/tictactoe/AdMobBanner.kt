a76e40a7c81acf626acab709efa6fa588207b0f3
alfonsoarcidiaconoeverli-dotcom <alfonsoarcidiaconoeverli@gmail.com>
2026-01-31 20:16:44 +0100

    Create AdMobBanner.kt

---
+++ app/src/main/java/com/org/tictactoe/AdMobBanner.kt
@@ -0,0 +1,30 @@
+package com.org.tictactoe
+
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdMobBanner(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    AndroidView(
+        modifier = modifier,
+        factory = {
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                adUnitId = "ca-app-pub-6445825465110921/6710007178"
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
+}
