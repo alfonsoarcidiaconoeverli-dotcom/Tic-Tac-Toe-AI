20bcc9e72fe039b700085372fce38ba3548251c4
alfonsoarcidiaconoeverli-dotcom <alfonsoarcidiaconoeverli@gmail.com>
2026-02-02 22:25:30 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.org.tictactoe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
